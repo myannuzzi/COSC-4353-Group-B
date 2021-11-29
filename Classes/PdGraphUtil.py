93ba9082479fcb6019e1a60c16899beff91e2f9b
cherishdayala <35199567+cherishdayala@users.noreply.github.com>
2021-11-29 17:19:58 -0600

    Update PdGraphUtil.py

--- Classes/PdGraphUtil.py
+++ Classes/PdGraphUtil.py
@@ -20,7 +20,7 @@ def adj_mat_to_df(adj_df, column_list):
       while col > 0:
         edge_list.append([index, headers[header_idx]])
         col -= 1
-      header_idx + 1
+      header_idx += 1
   df = pd.DataFrame(edge_list, columns=column_list)
   return df
 
@@ -58,7 +58,7 @@ def get_adj_mat(fileName, zero_indexed):
 
 # Convert Numpy adj matrix to csv (downloadable file)
 def adj_mat_to_csv(adj_mat, path=""):
-  np.savetxt(path + "adjMatrix.csv", adj_mat, delimiter=" ", fmt='%i')
+  np.savetxt(path + "adjMatrix.csv", adj_mat, delimiter=",", fmt='%i')
   
   
 # Converting graph.csv files to numpy adj matrix 
@@ -73,40 +73,12 @@ df = pd.DataFrame([["A", "B"], ["C", "A"], ["B", "D"], ["Z", "B"], ["A", "C"] , 
                       columns=["Column1", "Column2"])
 
 # Dataframe
-#print("Actual Dataframe")
+print("Actual Dataframe")
 print(df)
 print("\n")
 
-
-# CHERISH
-
-import numpy as np
-
-def get_edgelist_from_graph(fileName):
-  f = open(fileName, "r")
-  edge_list = []
-  for line in f.readlines():
-      node_list = line.strip().split('->')
-      from_node = node_list[0]
-      to_node_list = node_list[1].strip().split(',');
-      for node in to_node_list:
-        edge_list.append([int(from_node), int(node)])
-
-  return edge_list
-
-# This is the main function which he can use to convert file of any type either the csv or txt to numpy adj matrix
-def get_numpy_adjmat(fileName, zero_indexed = False):
-  if fileName.endswith('csv'):
-    adj_mat = np.genfromtxt(fileName, delimiter=',')
-    return  np.array(adj_mat, dtype=np.int)
-
-  else :
-    edge_list = get_edgelist_from_graph(fileName)
-    if not zero_indexed:
-      edge_list = [[x - 1 for x in edge] for edge in edge_list]
-
-    size = len(set([n for e in edge_list for n in e])) 
-    adjacency = [[0]*size for _ in range(size)]
-    for sink, source in edge_list:
-        adjacency[sink][source] += 1
-    return np.array(adjacency)
+# Converting dataframe to Adjacency Matrix
+df_adj = df_to_adj_matrix(df.Column1, df.Column2)
+print("Adjacency Matrix Dataframe")
+print(df_adj)
+print("\n")
