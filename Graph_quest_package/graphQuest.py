4d7bdc93dd2555c51d87dea9800712cb2fa38f50
Mike Yannuzzi (University of Houston) <43522450+myannuzzi@users.noreply.github.com>
2021-12-04 21:36:57 -0600

    wokring to copy over graph to test quest

--- Graph_quest_package/graphQuest.py
+++ Graph_quest_package/graphQuest.py
@@ -11,157 +11,157 @@ import sys
 
 # Graph Class
 class Graph:
-	# Class that represents the node and edge graph. Also contains functions for adding and removing 	nodes and edges, naming and storing graphs.
-	def __init__(self, name, adjMatrix, isWeighted, isDirected):
-		self.name = name
-        # Sets the np matrix
-		self.adjMatrix = adjMatrix
-        # Grabs the nodes and edges from matrix
-		self.nodeCount = self.getNodeCount()
-		self.edgeCount = self.getEdgeCount()
-        # Graph type defined by user
-		self.isWeighted = type(isWeighted)
-		self.isDirected = type(isDirected)
-        # Create list of nodes
-        nodeList = np.arange(1,self.getNodeCount())
+    # Class that represents the node and edge graph. Also contains functions for adding and removing 	nodes and edges, naming and storing graphs.
+    def __init__(self, name, adjMatrix, isWeighted, isDirected):
+        self.name = name
+    	# Sets the np matrix
+        self.adjMatrix = adjMatrix
+    	# Grabs the nodes and edges from matrix
+        self.nodeCount = self.getNodeCount()
+        self.edgeCount = self.getEdgeCount()
+    	# Graph type defined by user
+        self.isWeighted = type(isWeighted)
+        self.isDirected = type(isDirected)
+		
+    # Set name
+    # Set's name of graph
+    def setName(self, name):
+        self.name = name
 
-	# Set name
-	# Set's name of graph
-	def setName(self,name):
-		self.name = name
+    # Get name
+    # Get's name of graph
+    def getName(self):
+        return self.name
 
-	# Get name
-	# Get's name of graph
-	def getName(self):
-		return self.name
+    # Insert node
+    # Inserts node into graph (need to figure out about edge connections)
+    def insertNode(self):
+        copyMatrix = self.adjMatrix
+        # New idea, create an array of 0's one row and column bigger than the original and copy the values over
+        newMatrix = np.zeros(
+            (int(self.nodeCount+1), int(self.nodeCount+1)), dtype=int)
+        # loop through and add in values from previous matrix to new one
+        for i in range(self.nodeCount):
+            for j in range(self.nodeCount):
+                newMatrix[i][j] = copyMatrix[i][j]
+        # Set new matrix values
+        self.adjMatrix = newMatrix
 
-	# Insert node
-	# Inserts node into graph (need to figure out about edge connections)
-	def insertNode(self):
-		copyMatrix = self.adjMatrix
-		# New idea, create an array of 0's one row and column bigger than the original and copy the values over
-		newMatrix = np.zeros((int(self.nodeCount+1), int(self.nodeCount+1)),dtype=int)
-		# loop through and add in values from previous matrix to new one
-		for i in range(self.nodeCount):
-			for j in range(self.nodeCount):
-				newMatrix[i][j] = copyMatrix[i][j]
-		# Set new matrix values
-		self.adjMatrix = newMatrix
-			
-	# Remove node
-	# Removes node from graph (needs to remove related edges from node that was removed)
-	def removeNode(self,value):
-		# Remove both row and column from adjacency matrix
-		if(value < 0 or value > self.nodeCount):
-			print("Node out of bounds")
-			return -1
-		# Create new matrix
-		newMatrix = self.adjMatrix
-		# Delete row first
-		newMatrix = np.delete(newMatrix,value,0)
-		# print(newMatrix)
-		# Next delete column
-		newMatrix = np.delete(newMatrix,value,1)
-		# print(newMatrix)
-		print("Deleted node " + str(value))
-		self.adjMatrix = newMatrix
+    # Remove node
+    # Removes node from graph (needs to remove related edges from node that was removed)
+    def removeNode(self, value):
+        # Remove both row and column from adjacency matrix
+        if(value < 0 or value > self.nodeCount):
+            print("Node out of bounds")
+            return -1
+        # Create new matrix
+        newMatrix = self.adjMatrix
+        # Delete row first
+        newMatrix = np.delete(newMatrix, value, 0)
+        # print(newMatrix)
+        # Next delete column
+        newMatrix = np.delete(newMatrix, value, 1)
+        # print(newMatrix)
+        print("Deleted node " + str(value))
+        self.adjMatrix = newMatrix
 
     # Insert edge
-	# Inserts edge into graph (needs a source node and destination node, undirected or directed, can be self referencing)
-	def insertEdge(self, a, b):
-		# Check if the edge values are out of place first
-		if((a > self.nodeCount or a < 1) or (b > self.nodeCount or b < 1)):
-			print("Node index out of bounds")
-			return -1
+    # Inserts edge into graph (needs a source node and destination node, undirected or directed, can be self referencing)
+    def insertEdge(self, a, b):
+        # Check if the edge values are out of place first
+        if((a > self.nodeCount or a < 1) or (b > self.nodeCount or b < 1)):
+            print("Node index out of bounds")
+            return -1
 
-		newMatrix = self.adjMatrix
-		newMatrix[int(a-1),int(b-1)] = 1
-		newMatrix[int(b-1),int(a-1)] = 1
-		print("Edge added between nodes " + str(a) + " and " + str(b))
-		self.adjMatrix = newMatrix
+        newMatrix = self.adjMatrix
+        newMatrix[int(a-1), int(b-1)] = 1
+        newMatrix[int(b-1), int(a-1)] = 1
+        print("Edge added between nodes " + str(a) + " and " + str(b))
+        self.adjMatrix = newMatrix
 
-	# Remove edge
-	# Removes edge from between two nodes (needs node a, node b, and edge. Need to work on how multiple edges work)
-	def removeEdge(self, a, b):
-		# Sets edge value in matrix to 0
-		# Check if the edge values are out of place first
-		if (a > self.nodeCount or a < 1) or (b > self.nodeCount or b < 1):
-			print("Node index out of bounds")
-			return -1
-		
-		newMatrix = self.adjMatrix
-		newMatrix[int(a-1),int(b-1)] = 0
-		newMatrix[int(b-1),int(a-1)] = 0
-		print("Edge removed between nodes " + str(a) + " and " + str(b))
-		self.adjMatrix = newMatrix
+    # Remove edge
+    # Removes edge from between two nodes (needs node a, node b, and edge. Need to work on how multiple edges work)
+    def removeEdge(self, a, b):
+        # Sets edge value in matrix to 0
+        # Check if the edge values are out of place first
+        if (a > self.nodeCount or a < 1) or (b > self.nodeCount or b < 1):
+            print("Node index out of bounds")
+            return -1
 
-	# Set edge weight
-	# Sets the value and direction of an edge between two nodes (or one node if self referencing)
-	def setEdgeWeight(self, a, b, weight):
-		# CHECK IF WEIGHTED GRAPH
-		if(self.isWeighted != True):
-			print("Cannot be done on an unweighted graph...")
-			return
-		# Sets edge value in matrix to 0
-		# Check if the edge values are out of place first
-		if((a > self.nodeCount or a < 1) or (b > self.nodeCount or b < 1)):
-			print("Node index out of bounds")
-			return -1
-		
-		newMatrix = self.adjMatrix
-		newMatrix[int(a-1),int(b-1)] = weight
-		newMatrix[int(b-1),int(a-1)] = weight
-		print("Edge weight between nodes " + str(a) + " and " + str(b) + "changed to: " + str(weight))
-		self.adjMatrix = newMatrix
-	
-	# Get edge weight
-	# Get's edge weight between two nodes
-	def getEdgeWeight(self, a, b):
-		# CHECK IF WEIGHTED GRAPH
-		if(self.isWeighted != True):
-			print("Cannot be done on an unweighted graph...")
-			return
-		# Sets edge value in matrix to 0
-		# Check if the edge values are out of place first
-		if((a > self.nodeCount or a < 1) or (b > self.nodeCount or b < 1)):
-			print("Node index out of bounds")
-			return -1
-		weight = self.adjMatrix[int(a), int(b)]
-		return weight
+        newMatrix = self.adjMatrix
+        newMatrix[int(a-1), int(b-1)] = 0
+        newMatrix[int(b-1), int(a-1)] = 0
+        print("Edge removed between nodes " + str(a) + " and " + str(b))
+        self.adjMatrix = newMatrix
 
-	# NodeCount
-	# Gets count of number of nodes in graph
-	def getNodeCount(self):
-		# Call dim on passed in matrix to get node count
-		nodeCount = self.adjMatrix.shape
-		return nodeCount[0]
-		
-	# EdgeCount
-	# Gets count of number of nodes in graph
-	def getEdgeCount(self):
-		# Use numpy to grab the unique count of 1's and 0's
-		unique, count = np.unique(self.adjMatrix, return_counts = True)
-		d = dict(zip(unique, count))
-		edgeCount = int((d.get(1))/2)
-		return edgeCount
-	
-	# getMatrix
-	# Returns matrix for graph
-	def getMatrix(self):
-		newMatrix = self.adjMatrix
-		return newMatrix
+    # Set edge weight
+    # Sets the value and direction of an edge between two nodes (or one node if self referencing)
+    def setEdgeWeight(self, a, b, weight):
+        # CHECK IF WEIGHTED GRAPH
+        if(self.isWeighted != True):
+            print("Cannot be done on an unweighted graph...")
+            return
+        # Sets edge value in matrix to 0
+        # Check if the edge values are out of place first
+        if((a > self.nodeCount or a < 1) or (b > self.nodeCount or b < 1)):
+            print("Node index out of bounds")
+            return -1
+
+        newMatrix = self.adjMatrix
+        newMatrix[int(a-1), int(b-1)] = weight
+        newMatrix[int(b-1), int(a-1)] = weight
+        print("Edge weight between nodes " + str(a) +
+              " and " + str(b) + "changed to: " + str(weight))
+        self.adjMatrix = newMatrix
+
+    # Get edge weight
+    # Get's edge weight between two nodes
+    def getEdgeWeight(self, a, b):
+        # CHECK IF WEIGHTED GRAPH
+        if(self.isWeighted != True):
+            print("Cannot be done on an unweighted graph...")
+            return
+        # Sets edge value in matrix to 0
+        # Check if the edge values are out of place first
+        if((a > self.nodeCount or a < 1) or (b > self.nodeCount or b < 1)):
+            print("Node index out of bounds")
+            return -1
+        weight = self.adjMatrix[int(a), int(b)]
+        return weight
+
+    # NodeCount
+    # Gets count of number of nodes in graph
+    def getNodeCount(self):
+        # Call dim on passed in matrix to get node count
+        nodeCount = self.adjMatrix.shape
+        return nodeCount[0]
+
+    # EdgeCount
+    # Gets count of number of nodes in graph
+    def getEdgeCount(self):
+        # Use numpy to grab the unique count of 1's and 0's
+        unique, count = np.unique(self.adjMatrix, return_counts=True)
+        d = dict(zip(unique, count))
+        edgeCount = int((d.get(1))/2)
+        return edgeCount
+
+    # getMatrix
+    # Returns matrix for graph
+    def getMatrix(self):
+        newMatrix = self.adjMatrix
+        return newMatrix
 
     # getDirection
     # Gets the direction of a directed edge between two nodes
     # def getDirection(self, a, b):
     #     pass
 
-	# # Output function
-	# # Create a csv file
-	# def save(self):
-	# 	# Put numpy array into csv file
-	# 	print("Saving file under 'adjmatrix.csv'")
-	# 	np.savetxt('adjMatrix.csv', self.adjMatrix, delimiter=",")
+    # # Output function
+    # # Create a csv file
+    # def save(self):
+    # 	# Put numpy array into csv file
+    # 	print("Saving file under 'adjmatrix.csv'")
+    # 	np.savetxt('adjMatrix.csv', self.adjMatrix, delimiter=",")
 
 
 # # Graph Algorithms
@@ -260,7 +260,7 @@ class Graph:
 # given_file = 1
 
 # # given_num = int(input("enter a number between 0 to 4 to search in the graph: "))
-# given_num = 1 
+# given_num = 1
 
 # #  for the purpose of testing, instead of taking user input I am passing an integer
 
@@ -544,5 +544,3 @@ class Graph:
 # def adj_mat_to_csv(adj_mat, path=""):
 #     np.savetxt(path + "adjMatrix.csv", adj_mat, delimiter=",", fmt='%i')
 #     print("The output will be stored outside the directory in a file called adjMatrix.csv")
-
-
