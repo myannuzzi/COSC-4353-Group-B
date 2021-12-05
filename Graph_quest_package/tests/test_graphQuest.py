2df1d10810165cd5619b6fb5b83a10618c055fa3
Mike Yannuzzi (University of Houston) <43522450+myannuzzi@users.noreply.github.com>
2021-12-05 15:21:10 -0600

    added setup and teardown and print to test file

--- Graph_quest_package/tests/test_graphQuest.py
+++ Graph_quest_package/tests/test_graphQuest.py
@@ -6,214 +6,102 @@ import os
 
 
 class TestGraphQuestPackage(unittest.TestProgram):
-    def test_constructor(self):
-        adjMatrix = np.array([[0,1,1,0,0],
-							[1,0,1,1,1],
-							[1,1,0,1,0],
-							[0,1,1,0,1],
-							[0,1,0,1,0]])
-        test = Graph("TestGraph", adjMatrix, False, False)
+    # Adding in setup and teardown to reduce code reusage
+    def setUp(self):
+        print("setup")
+        adjMatrix = np.array([[0, 1, 1, 0, 0],
+                              [1, 0, 1, 1, 1],
+                              [1, 1, 0, 1, 0],
+                              [0, 1, 1, 0, 1],
+                              [0, 1, 0, 1, 0]])
+        self.test = Graph("TestGraph", adjMatrix, False, False)
 
+    def tearDown(self):
+        print("teardown")
+        pass
+
+    def test_constructor(self):
+        print("test_constructor")
         pass
 
     def test_getName(self):
-        adjMatrix = np.array([[0,1,1,0,0],
-							[1,0,1,1,1],
-							[1,1,0,1,0],
-							[0,1,1,0,1],
-							[0,1,0,1,0]])
-        test = Graph("TestGraph", adjMatrix, False, False)
-
+        print("test_getName")
         pass
 
     def test_setName(self):
-        adjMatrix = np.array([[0,1,1,0,0],
-							[1,0,1,1,1],
-							[1,1,0,1,0],
-							[0,1,1,0,1],
-							[0,1,0,1,0]])
-        test = Graph("TestGraph", adjMatrix, False, False)
-
+        print("test_setName")
         pass
 
     def test_isWeighted(self):
-        adjMatrix = np.array([[0,1,1,0,0],
-							[1,0,1,1,1],
-							[1,1,0,1,0],
-							[0,1,1,0,1],
-							[0,1,0,1,0]])
-        test = Graph("TestGraph", adjMatrix, False, False)
-
+        print("test_isWeighted")
         pass
 
     def test_isDirected(self):
-        adjMatrix = np.array([[0,1,1,0,0],
-							[1,0,1,1,1],
-							[1,1,0,1,0],
-							[0,1,1,0,1],
-							[0,1,0,1,0]])
-        test = Graph("TestGraph", adjMatrix, False, False)
-
+        print("test_isDirected")
         pass
 
     def test_getNodeCount(self):
-        adjMatrix = np.array([[0,1,1,0,0],
-							[1,0,1,1,1],
-							[1,1,0,1,0],
-							[0,1,1,0,1],
-							[0,1,0,1,0]])
-        test = Graph("TestGraph", adjMatrix, False, False)
-
+        print("test_getNodeCount")
         pass
 
     def test_getEdgeCount(self):
-        adjMatrix = np.array([[0,1,1,0,0],
-							[1,0,1,1,1],
-							[1,1,0,1,0],
-							[0,1,1,0,1],
-							[0,1,0,1,0]])
-        test = Graph("TestGraph", adjMatrix, False, False)
-
+        print("test_getEdgeCount")
         pass
 
     def test_getAdjMatrix(self):
-        adjMatrix = np.array([[0,1,1,0,0],
-							[1,0,1,1,1],
-							[1,1,0,1,0],
-							[0,1,1,0,1],
-							[0,1,0,1,0]])
-        test = Graph("TestGraph", adjMatrix, False, False)
-
+        print("test_getAdjMatrix")
         pass
 
     def test_getAdjList(self):
-        adjMatrix = np.array([[0,1,1,0,0],
-							[1,0,1,1,1],
-							[1,1,0,1,0],
-							[0,1,1,0,1],
-							[0,1,0,1,0]])
-        test = Graph("TestGraph", adjMatrix, False, False)
-
+        print("test_getAdjList")
         pass
 
     def test_getNodeList(self):
-        adjMatrix = np.array([[0,1,1,0,0],
-							[1,0,1,1,1],
-							[1,1,0,1,0],
-							[0,1,1,0,1],
-							[0,1,0,1,0]])
-        test = Graph("TestGraph", adjMatrix, False, False)
-
+        print("test_getNodeList")
         pass
 
     def test_getEdgeList(self):
-        adjMatrix = np.array([[0,1,1,0,0],
-							[1,0,1,1,1],
-							[1,1,0,1,0],
-							[0,1,1,0,1],
-							[0,1,0,1,0]])
-        test = Graph("TestGraph", adjMatrix, False, False)
-
+        print("test_getEdgeList")
         pass
 
     def test_createAdjList(self):
-        adjMatrix = np.array([[0,1,1,0,0],
-							[1,0,1,1,1],
-							[1,1,0,1,0],
-							[0,1,1,0,1],
-							[0,1,0,1,0]])
-        test = Graph("TestGraph", adjMatrix, False, False)
-
+        print("test_createAdjList")
         pass
 
     def test_createNodeList(self):
-        adjMatrix = np.array([[0,1,1,0,0],
-							[1,0,1,1,1],
-							[1,1,0,1,0],
-							[0,1,1,0,1],
-							[0,1,0,1,0]])
-        test = Graph("TestGraph", adjMatrix, False, False)
-
+        print("test_createNodeList")
         pass
 
     def test_insertNode(self):
-        adjMatrix = np.array([[0,1,1,0,0],
-							[1,0,1,1,1],
-							[1,1,0,1,0],
-							[0,1,1,0,1],
-							[0,1,0,1,0]])
-        test = Graph("TestGraph", adjMatrix, False, False)
-
+        print("test_insertNode")
         pass
 
     def test_removeNode(self):
-        adjMatrix = np.array([[0,1,1,0,0],
-							[1,0,1,1,1],
-							[1,1,0,1,0],
-							[0,1,1,0,1],
-							[0,1,0,1,0]])
-        test = Graph("TestGraph", adjMatrix, False, False)
-
+        print("test_removeNode")
         pass
 
     def test_insertEdge(self):
-        adjMatrix = np.array([[0,1,1,0,0],
-							[1,0,1,1,1],
-							[1,1,0,1,0],
-							[0,1,1,0,1],
-							[0,1,0,1,0]])
-        test = Graph("TestGraph", adjMatrix, False, False)
-
+        print("test_insertEdge")
         pass
 
     def test_removeEdge(self):
-        adjMatrix = np.array([[0,1,1,0,0],
-							[1,0,1,1,1],
-							[1,1,0,1,0],
-							[0,1,1,0,1],
-							[0,1,0,1,0]])
-        test = Graph("TestGraph", adjMatrix, False, False)
-
+        print("test_removeEdge")
         pass
 
     def test_getEdgeWeight(self):
-        adjMatrix = np.array([[0,1,1,0,0],
-							[1,0,1,1,1],
-							[1,1,0,1,0],
-							[0,1,1,0,1],
-							[0,1,0,1,0]])
-        test = Graph("TestGraph", adjMatrix, False, False)
-
+        print("test_getEdgeWeight")
         pass
-    
-    def test_setEdgeWeight(self):
-        adjMatrix = np.array([[0,1,1,0,0],
-							[1,0,1,1,1],
-							[1,1,0,1,0],
-							[0,1,1,0,1],
-							[0,1,0,1,0]])
-        test = Graph("TestGraph", adjMatrix, False, False)
 
+    def test_setEdgeWeight(self):
+        print("test_setEdgeWeight")
         pass
 
     def test_getEdgeDirection(self):
-        adjMatrix = np.array([[0,1,1,0,0],
-							[1,0,1,1,1],
-							[1,1,0,1,0],
-							[0,1,1,0,1],
-							[0,1,0,1,0]])
-        test = Graph("TestGraph", adjMatrix, False, False)
-
+        print("test_getEdgeDirection")
         pass
 
     def test_setEdgeDirection(self):
-        adjMatrix = np.array([[0,1,1,0,0],
-							[1,0,1,1,1],
-							[1,1,0,1,0],
-							[0,1,1,0,1],
-							[0,1,0,1,0]])
-        test = Graph("TestGraph", adjMatrix, False, False)
-
+        print("test_setEdgeDirection")
         pass
 
     # Add other test functions below
