62d28fdaf8fe73cabaa3f6840b467834334e3795
Mike Yannuzzi (University of Houston) <43522450+myannuzzi@users.noreply.github.com>
2021-12-04 22:09:29 -0600

    adding in function to create adjlist in graph class

--- Graph_quest_package/testQuest.py
+++ Graph_quest_package/testQuest.py
@@ -22,7 +22,7 @@ class Graph:
         self.isDirected = type(isDirected)
         # Create the node list
         self.nodeList = self.createNodeList(self, self.nodeCount)
-        pass
+        self.adjList = self.createAdjList(self, adjMatrix)
     
     # createNodeList
     # Creates the node list for the graph constructor
@@ -175,62 +175,69 @@ class Graph:
     def getNodeList(self):
         return self.nodeList
 
+    # createAdjList
+    # Creates adjacency list from adjcency matrix
+    def createAdjList(self):
+        pass
+
 # Algorithm function sections
-# ------------------BFS-----------------------
-# input_folder = 'Adjacency matrices'
+# # ------------------BFS-----------------------
+# # input_folder = 'Adjacency matrices'
 
-# inputs_list = []
+# # inputs_list = []
 
-# for f1, f2, f3 in os.walk(input_folder):
-#     for file in f3:
-#         inputs_list.append(os.path.join(f1, file))
+# # for f1, f2, f3 in os.walk(input_folder):
+# #     for file in f3:
+# #         inputs_list.append(os.path.join(f1, file))
 
-# print(inputs_list)
-# # given_file = int(input("choose one file from the available inputs as a number starting with 1: "))
-# given_file = 1
+# # print(inputs_list)
+# # # given_file = int(input("choose one file from the available inputs as a number starting with 1: "))
+# # given_file = 1
 
-# # given_num = int(input("enter a number between 0 to 4 to search in the graph: "))
-# given_num = 1 
+# # # given_num = int(input("enter a number between 0 to 4 to search in the graph: "))
+# # given_num = 1 
 
-# #  for the purpose of testing, instead of taking user input I am passing an integer
-
-
-# g = file_to_mat.file_to_matrix(input_folder, given_file)
-# # print(g)
+# # #  for the purpose of testing, instead of taking user input I am passing an integer
 
 
-def matrix_to_list(matrix_input):
-    graph = {}
-    for i, node in enumerate(matrix_input):
-        adj = []
-        for j, connected in enumerate(node):
-            if connected:
-                adj.append(j)
-        graph[i] = adj
-    return graph
+# # g = file_to_mat.file_to_matrix(input_folder, given_file)
+# # # print(g)
 
 
-# matrix = [[0, 19, 5, 0, 0],
-#           [19, 0, 5, 9, 2],
-#           [5, 5, 0, 1, 6],
-#           [0, 9, 1, 0, 1],
-#           [0, 2, 6, 1, 0]]
+# def matrix_to_list(matrix_input):
+#     graph = {}
+#     for i, node in enumerate(matrix_input):
+#         adj = []
+#         for j, connected in enumerate(node):
+#             if connected:
+#                 adj.append(j)
+#         graph[i] = adj
+#     return graph
 
 
-def bfs(graph, v):
-    visited = []
-    Q = [v]
-    while Q:
-        v = Q.pop(0)
-        visited.append(v)
-        for n in graph[v]:
-            if n not in Q and \
-                    n not in visited:
-                Q.append(n)
-    return visited
+# # matrix = [[0, 19, 5, 0, 0],
+# #           [19, 0, 5, 9, 2],
+# #           [5, 5, 0, 1, 6],
+# #           [0, 9, 1, 0, 1],
+# #           [0, 2, 6, 1, 0]]
 
 
-gr = matrix_to_list(g)
-# print(gr)
-print("The breadth first search result on the given number is: ")
-print(bfs(gr, given_num))
+# def bfs(graph, v):
+#     visited = []
+#     Q = [v]
+#     while Q:
+#         v = Q.pop(0)
+#         visited.append(v)
+#         for n in graph[v]:
+#             if n not in Q and \
+#                     n not in visited:
+#                 Q.append(n)
+#     return visited
+
+
+# gr = matrix_to_list(g)
+# # print(gr)
+# print("The breadth first search result on the given number is: ")
+# print(bfs(gr, given_num))
+
+#-------------------------PD----------------------------
